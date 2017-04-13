58e9aad39c9a3282adc08a4d85cc11ed286f757f
Cbinbin <1142293069@qq.com>
2017-04-13 16:39:19 +0800

    ws

--- express.js
+++ express.js
@@ -1,42 +1,36 @@
-'use strict'
-
+const express = require('express');
+const https = require('https');
+const url = require('url');
+const WebSocket = require('ws');
 var fs = require('fs');
-var https = require('https');
-
-var server = https.createServer(
+ 
+const app = express();
+ 
+app.use(function (req, res) {
+  res.send({ msg: "hello" });
+});
+ 
+const server = https.createServer(
     {
       'key':fs.readFileSync('cbinbin.key'),
       'cert':fs.readFileSync('cbinbin.crt')
     }
   );
-var  url = require('url');
-var  WebSocketServer = require('ws').Server;
-var  wss = new WebSocketServer({
-        server: server
-      });
-var express = require('express');
-var app = express();
-var port = 8090;
-
-app.use(function(req, res) {
-    res.send({
-        msg: "hello"
-    });
-});
-
+const wss = new WebSocket.Server({ server });
+ 
 wss.on('connection', function connection(ws) {
-    var location = url.parse(ws.upgradeReq.url, true);
-    // you might use location.query.access_token to authenticate or share sessions
-    // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-    });
-
-    ws.send('something');
+  const location = url.parse(ws.upgradeReq.url, true);
+  // You might use location.query.access_token to authenticate or share sessions 
+  // or ws.upgradeReq.headers.cookie (see https://stackoverflow.com/a/16395220/151312) 
+ 
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+ 
+  ws.send('something');
 });
 
 server.on('request', app);
-server.listen(port, function() {
-    console.log('Listening on ' + server.address().port)
+server.listen(8090, function listening() {
+  console.log('Listening on %d', server.address().port);
 });
