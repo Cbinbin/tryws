f9154d74819e0f286bb657da0b5c0d174668fb5b
Cbinbin <1142293069@qq.com>
2017-04-14 15:29:52 +0800

    backups

--- express.js
+++ express.js
@@ -11,17 +11,20 @@ app.use(function (req, res) {
  
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
- 
-wss.on('connection', function connection(ws) {
+
+wss.on('connection', (ws)=> {
   const location = url.parse(ws.upgradeReq.url, true);
   // You might use location.query.access_token to authenticate or share sessions 
   // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312) 
- 
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on('message', (data)=> {
+    wss.clients.forEach((client)=> {
+      if (client.readyState === WebSocket.OPEN) {    //client !== ws && 
+        client.send(data);
+      } else console.log('Client:' + data)
+    });
   });
  
-  ws.send('something');
+  ws.send('Welcome to room!');
 });
  
 server.listen(8090, function listening() {
---
+++ yyh_api_1/routes/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  home: require('./home'),
+  // admin: require('./admin'),
+  // wechat: require('./wechat'),
+  // wechatpay: require('./wechatpay'),
+  // period: require('./period'),
+  // field: require('./field'),
+  // site: require('./site'),
+  // place: require('./place')
+}
